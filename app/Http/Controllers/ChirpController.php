7c9aca717aa84de59f1a3092ee0fdf32f76c8820
Jabez H. <jabezhm@gmail.com>
2024-06-02 10:22:11 +0300

    feat: create and view chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Response;
+use Inertia\Inertia;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        $chirps = Chirp::with('user:id,name')->latest()->get();
+        return Inertia::render('Chirps/Index', [
+            'chirps' => $chirps,
+        ]);
     }
 
     /**
@@ -26,9 +32,15 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
+    }
 
     /**
      * The attributes that are mass assignable.
